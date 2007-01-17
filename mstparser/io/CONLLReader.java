392995794d83ece52492a5b92980100f4751f762
jasonbaldridge <jasonbaldridge@de088c05-6c1f-0410-98de-a98a717ad882>
2007-01-17 17:05:12 +0000

    Added documentation.
    git-svn-id: https://mstparser.svn.sourceforge.net/svnroot/mstparser/trunk/mstparser@94 de088c05-6c1f-0410-98de-a98a717ad882

--- mstparser/io/CONLLReader.java
+++ mstparser/io/CONLLReader.java
@@ -1,3 +1,15 @@
+///////////////////////////////////////////////////////////////////////////////
+// Copyright (C) 2007 University of Texas at Austin and (C) 2005
+// University of Pennsylvania and Copyright (C) 2002, 2003 University
+// of Massachusetts Amherst, Department of Computer Science.
+//
+// This software is licensed under the terms of the Common Public
+// License, Version 1.0 or (at your option) any subsequent version.
+// 
+// The license is approved by the Open Source Initiative, and is
+// available from their website at http://www.opensource.org.
+///////////////////////////////////////////////////////////////////////////////
+
 package mstparser.io;
 
 import mstparser.DependencyInstance;
@@ -7,6 +19,17 @@ import mstparser.Util;
 import java.io.*;
 import java.util.*;
 
+/**
+ * A reader for files in CoNLL format.
+ *
+ * <p>
+ * Created: Sat Nov 10 15:25:10 2001
+ * </p>
+ *
+ * @author Jason Baldridge
+ * @version $Id$
+ * @see mstparser.io.DependencyReader
+ */
 public class CONLLReader extends DependencyReader {
 
 
--- mstparser/io/DependencyWriter.java
+++ mstparser/io/DependencyWriter.java
@@ -1,8 +1,31 @@
+///////////////////////////////////////////////////////////////////////////////
+// Copyright (C) 2007 University of Texas at Austin and (C) 2005
+// University of Pennsylvania and Copyright (C) 2002, 2003 University
+// of Massachusetts Amherst, Department of Computer Science.
+//
+// This software is licensed under the terms of the Common Public
+// License, Version 1.0 or (at your option) any subsequent version.
+// 
+// The license is approved by the Open Source Initiative, and is
+// available from their website at http://www.opensource.org.
+///////////////////////////////////////////////////////////////////////////////
+
 package mstparser.io;
 
 import java.io.*;
 import mstparser.*;
 
+/**
+ * A class that defines common behavior and abstract methods for
+ * writers for different formats.
+ *
+ * <p>
+ * Created: Sat Nov 10 15:25:10 2001
+ * </p>
+ *
+ * @author Jason Baldridge
+ * @version $Id$
+ */
 public abstract class DependencyWriter {
 
     protected BufferedWriter writer;
